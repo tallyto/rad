9d68d7adaf22d3edc3b32cc99ddee742894b4dbe
tallyto.rodrigues <tallyto.rodrigues@senior.com.br>
2023-09-04 20:28:59 -0300

    feat: translate to portuguese

--- tic-tac-toe/app.py
+++ tic-tac-toe/app.py
@@ -2,68 +2,68 @@ import tkinter as tk
 from tkinter import messagebox
 
 # Função para verificar se há um vencedor
-def check_winner(board):
+def verificar_vencedor(tabuleiro):
     # Verifica se alguém ganhou na horizontal, vertical ou diagonal
     for i in range(3):
-        if board[i][0] == board[i][1] == board[i][2] != "":
-            return board[i][0]
-        if board[0][i] == board[1][i] == board[2][i] != "":
-            return board[0][i]
+        if tabuleiro[i][0] == tabuleiro[i][1] == tabuleiro[i][2] != "":
+            return tabuleiro[i][0]
+        if tabuleiro[0][i] == tabuleiro[1][i] == tabuleiro[2][i] != "":
+            return tabuleiro[0][i]
     
-    if board[0][0] == board[1][1] == board[2][2] != "":
-        return board[0][0]
-    if board[0][2] == board[1][1] == board[2][0] != "":
-        return board[0][2]
+    if tabuleiro[0][0] == tabuleiro[1][1] == tabuleiro[2][2] != "":
+        return tabuleiro[0][0]
+    if tabuleiro[0][2] == tabuleiro[1][1] == tabuleiro[2][0] != "":
+        return tabuleiro[0][2]
     
     return None
 
 # Função chamada quando um botão do tabuleiro é clicado
-def on_click(row, col):
-    global current_player, board, winner
+def ao_clicar(linha, coluna):
+    global jogador_atual, tabuleiro, vencedor
 
     # Verifica se a célula está vazia e não há vencedor
-    if board[row][col] == "" and winner is None:
-        board[row][col] = current_player
-        buttons[row][col].config(text=current_player)
+    if tabuleiro[linha][coluna] == "" and vencedor is None:
+        tabuleiro[linha][coluna] = jogador_atual
+        botoes[linha][coluna].config(text=jogador_atual)
         
         # Verifica se há um vencedor
-        winner = check_winner(board)
-        if winner:
-            messagebox.showinfo("Tic-Tac-Toe", f"Jogador {winner} venceu!")
+        vencedor = verificar_vencedor(tabuleiro)
+        if vencedor:
+            messagebox.showinfo("Jogo da Velha", f"O jogador {vencedor} venceu!")
             root.quit()
         # Verifica empate
-        elif all(board[i][j] != "" for i in range(3) for j in range(3)):
-            messagebox.showinfo("Tic-Tac-Toe", "Empate!")
+        elif all(tabuleiro[i][j] != "" for i in range(3) for j in range(3)):
+            messagebox.showinfo("Jogo da Velha", "Empate!")
             root.quit()
         
         # Alterna o jogador atual
-        current_player = "X" if current_player == "O" else "O"
+        jogador_atual = "X" if jogador_atual == "O" else "O"
 
 # Cria a janela principal
 root = tk.Tk()
 root.geometry("550x550")  # Define o tamanho da janela
-root.title("Tic-Tac-Toe")
+root.title("Jogo da Velha")
 
 # Cria o quadro principal
-main_frame = tk.Frame(root, border=3, relief='groove')
-main_frame.pack(expand=True, fill="both")
+quadro_principal = tk.Frame(root, border=3, relief='groove')
+quadro_principal.pack(expand=True, fill="both")
 
 # Inicializa a matriz do tabuleiro, o jogador atual e a variável de vencedor
-board = [["" for _ in range(3)] for _ in range(3)]
-current_player = "X"
-winner = None
+tabuleiro = [["" for _ in range(3)] for _ in range(3)]
+jogador_atual = "X"
+vencedor = None
 
-# Cria os botões do tabuleiro e associa a função on_click
-buttons = []
+# Cria os botões do tabuleiro e associa a função ao_clicar
+botoes = []
 for i in range(3):
-    row_buttons = []
+    botoes_linha = []
     for j in range(3):
-        button = tk.Button(main_frame, text="", border=1, relief="groove", padx=20, pady=20,
-                           width=10, height=3,  # Ajuste o tamanho dos botões aqui
-                           command=lambda row=i, col=j: on_click(row, col))
-        button.grid(row=i, column=j)
-        row_buttons.append(button)
-    buttons.append(row_buttons)
+        botao = tk.Button(quadro_principal, text="", border=1, relief="groove", padx=20, pady=20,
+                          width=10, height=3,  # Ajuste o tamanho dos botões aqui
+                          command=lambda linha=i, coluna=j: ao_clicar(linha, coluna))
+        botao.grid(row=i, column=j)
+        botoes_linha.append(botao)
+    botoes.append(botoes_linha)
 
 # Inicia o loop principal da interface gráfica
 root.mainloop()
