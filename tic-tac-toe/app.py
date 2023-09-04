2aad49df16bd60e7121ea9d3be3b9d2a38a8350f
tallyto.rodrigues <tallyto.rodrigues@senior.com.br>
2023-09-04 19:46:47 -0300

    feat: gerando tabuleiro

--- tic-tac-toe/app.py
+++ tic-tac-toe/app.py
@@ -1,10 +1,14 @@
 import tkinter as tk
 
 root = tk.Tk()
+root.geometry("550x550")
+
 main_frame = tk.Frame(root, border=3, relief='groove')
-root.geometry(newGeometry="550x550")
+main_frame.pack(expand=True, fill="both")  # Usando pack para expandir e preencher
 
 for i in range(3):
-    tk.Label(main_frame, text="AAAAAAAAAAAA", border=1, relief="groove",
-             padx=20, pady=20).grid(column=0, row=i)
+    for j in range(3):
+        tk.Label(main_frame, text="A", border=1, relief="groove",
+                 padx=20, pady=20).grid(row=i, column=j)
+
 root.mainloop()
