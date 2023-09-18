7fb098658e6abac6349a386cbdab1a4461c25481
tallyto <rodrigues.tallyto@hotmail.com>
2023-09-18 19:36:18 -0300

    Cria label resultado e botão sair

--- file/app.py
+++ file/app.py
@@ -2,6 +2,10 @@ import tkinter as tk
 class App (tk.Frame):
     # Construtor da Sub Classe
     def __init__ (self):
+        self.msg: tk.Label
+        self.botaoBuscar: tk.Button
+        self.msgResultado: tk.Label
+        self.botaoSair: tk.Button
         # Construtor da Super Classe
         tk.Frame.__init__(self)
         print("Olá")
@@ -18,6 +22,11 @@ class App (tk.Frame):
         self.butaoBuscar["width"] = 30
         self.butaoBuscar["command"] = self.quit
         self.butaoBuscar.pack()
+
+        self.msgResultado = tk.Label(self.master, text="Resultado após clicar.")
+        
+        self.botaoSair = tk.Button(self.master, text="Sair" , bg="red", fg="white", width="20", command= self.quit)
+        self.botaoSair.pack()
 
 obj = App()
 obj.mainloop()
