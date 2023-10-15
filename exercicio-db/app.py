c31c55d2751b8f1723aa3b3f5598d3f3ef24666c
tallyto <rodrigues.tallyto@hotmail.com>
2023-10-14 23:18:55 -0300

    adicionado matricula

--- exercicio-db/app.py
+++ exercicio-db/app.py
@@ -2,7 +2,6 @@ import tkinter as tk
 from tkinter import messagebox
 import mysql.connector
 
-
 db = mysql.connector.connect(
     host="127.0.0.1",
     user="root",
@@ -18,7 +17,7 @@ def criaTabela(cursor):
     try:
         createTable = '''
         CREATE TABLE IF NOT EXISTS aluno (
-            id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+            matricula VARCHAR(10) PRIMARY KEY,
             nome VARCHAR(50),
             av1 DECIMAL(5, 2),
             av2 DECIMAL(5, 2),
@@ -34,15 +33,15 @@ def criaTabela(cursor):
     except Exception as e:
         print("Erro ao criar a tabela:", e)
 
-
 # Função para adicionar um aluno
 def adicionar_aluno():
+    matricula = entry_matricula.get()
     nome = entry_nome.get()
     av1 = entry_av1.get()
     av2 = entry_av2.get()
     av3 = entry_av3.get()
     criado_por = entry_criado_por.get()
-    cursor.execute("INSERT INTO aluno (nome, av1, av2, av3, criadoPor) VALUES (%s, %s, %s, %s, %s)", (nome, av1, av2, av3, criado_por))
+    cursor.execute("INSERT INTO aluno (matricula, nome, av1, av2, av3, criadoPor) VALUES (%s, %s, %s, %s, %s, %s)", (matricula, nome, av1, av2, av3, criado_por))
     db.commit()
     listar_alunos()
 
@@ -54,7 +53,7 @@ def listar_alunos():
 
     for aluno in alunos:
         media = (aluno[2] + aluno[3] + aluno[4]) / 3
-        resultado_text.insert(tk.END, f"ID: {aluno[0]}\n")
+        resultado_text.insert(tk.END, f"Matrícula: {aluno[0]}\n")
         resultado_text.insert(tk.END, f"Nome: {aluno[1]}\n")
         resultado_text.insert(tk.END, f"AV1: {aluno[2]}\n")
         resultado_text.insert(tk.END, f"AV2: {aluno[3]}\n")
@@ -64,38 +63,38 @@ def listar_alunos():
         resultado_text.insert(tk.END, "\n")
 
         # Adicione botões "Editar" e "Remover" para cada aluno
-        btn_editar = tk.Button(root, text="Editar", command=lambda aluno_id=aluno[0]: editar_aluno(aluno_id))
-        btn_remover = tk.Button(root, text="Remover", command=lambda aluno_id=aluno[0]: remover_aluno(aluno_id))
+        btn_editar = tk.Button(root, text="Editar", command=lambda matricula=aluno[0]: editar_aluno(matricula))
+        btn_remover = tk.Button(root, text="Remover", command=lambda matricula=aluno[0]: remover_aluno(matricula))
         resultado_text.window_create(tk.END, window=btn_editar)
         resultado_text.window_create(tk.END, window=btn_remover)
         resultado_text.insert(tk.END, "\n")
 
 # Função para remover um aluno
-def remover_aluno(aluno_id):
+def remover_aluno(matricula):
     # Confirmação de remoção
     if messagebox.askyesno("Confirmação", "Tem certeza de que deseja remover este aluno?"):
-        cursor.execute("DELETE FROM aluno WHERE id = %s", (aluno_id,))
+        cursor.execute("DELETE FROM aluno WHERE matricula = %s", (matricula,))
         db.commit()
         listar_alunos()
 
 # Função para editar um aluno
-def editar_aluno(aluno_id):
+def editar_aluno(matricula):
     # Função para salvar as alterações no aluno
     def salvar_alteracoes():
         novo_nome = entry_novo_nome.get()
         nova_av1 = entry_nova_av1.get()
         nova_av2 = entry_nova_av2.get()
         nova_av3 = entry_nova_av3.get()
-        cursor.execute("UPDATE aluno SET nome = %s, av1 = %s, av2 = %s, av3 = %s WHERE id = %s", (novo_nome, nova_av1, nova_av2, nova_av3, aluno_id))
+        cursor.execute("UPDATE aluno SET nome = %s, av1 = %s, av2 = %s, av3 = %s WHERE matricula = %s", (novo_nome, nova_av1, nova_av2, nova_av3, matricula))
         db.commit()
         listar_alunos()
         popup.destroy()
 
-    # Obtenha o ID do aluno selecionado (passado como argumento)
-    aluno_id = aluno_id
+    # Obtenha a matrícula do aluno selecionado (passado como argumento)
+    matricula = matricula
 
-    # Consulte o aluno pelo ID
-    cursor.execute("SELECT * FROM aluno WHERE id = %s", (aluno_id,))
+    # Consulte o aluno pela matrícula
+    cursor.execute("SELECT * FROM aluno WHERE matricula = %s", (matricula,))
     aluno = cursor.fetchone()
 
     # Criação da janela popup para edição
@@ -131,8 +130,6 @@ def editar_aluno(aluno_id):
     entry_nova_av3.pack()
     btn_salvar.pack()
 
-
-
 # Configuração da janela principal
 root = tk.Tk()
 root.title("Cadastro de Alunos")
@@ -141,6 +138,8 @@ root.title("Cadastro de Alunos")
 criaTabela(cursor)
 
 # Labels e campos de entrada
+label_matricula = tk.Label(root, text="Matrícula:")
+entry_matricula = tk.Entry(root)
 label_nome = tk.Label(root, text="Nome:")
 entry_nome = tk.Entry(root)
 label_av1 = tk.Label(root, text="AV1:")
@@ -152,7 +151,6 @@ entry_av3 = tk.Entry(root)
 label_criado_por = tk.Label(root, text="Criado por:")
 entry_criado_por = tk.Entry(root)
 
-
 # Botão para adicionar aluno
 btn_adicionar = tk.Button(root, text="Adicionar Aluno", command=adicionar_aluno)
 
@@ -161,18 +159,20 @@ resultado_text = tk.Text(root)
 resultado_text.config(state=tk.NORMAL)
 
 # Organização dos elementos na janela
-label_nome.grid(row=0, column=0)
-entry_nome.grid(row=0, column=1)
-label_av1.grid(row=1, column=0)
-entry_av1.grid(row=1, column=1)
-label_av2.grid(row=2, column=0)
-entry_av2.grid(row=2, column=1)
-label_av3.grid(row=3, column=0)
-entry_av3.grid(row=3, column=1)
-label_criado_por.grid(row=4, column=0)
-entry_criado_por.grid(row=4, column=1)
-btn_adicionar.grid(row=5, columnspan=2)
-resultado_text.grid(row=6, columnspan=2)
+label_matricula.grid(row=0, column=0)
+entry_matricula.grid(row=0, column=1)
+label_nome.grid(row=1, column=0)
+entry_nome.grid(row=1, column=1)
+label_av1.grid(row=2, column=0)
+entry_av1.grid(row=2, column=1)
+label_av2.grid(row=3, column=0)
+entry_av2.grid(row=3, column=1)
+label_av3.grid(row=4, column=0)
+entry_av3.grid(row=4, column=1)
+label_criado_por.grid(row=5, column=0)
+entry_criado_por.grid(row=5, column=1)
+btn_adicionar.grid(row=6, columnspan=2)
+resultado_text.grid(row=7, columnspan=2)
 
 # Listar alunos ao iniciar a aplicação
 listar_alunos()
